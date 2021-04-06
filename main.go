c9baa606cf2759a683fb7144c1c39beb06746c37
Kristian Nissen <kristian.nissen@gmail.com>
2021-04-06 09:54:32 +0200

    Things

--- main.go
+++ main.go
@@ -1,57 +1,9 @@
 package main
 
 import (
-	"funwithwords/stemmer"
-	"funwithwords/stopword"
-	"io/ioutil"
 	"log"
-	"os"
-	"path/filepath"
-	"strings"
 )
 
-const dataset string = "./dataset"
-
 func main() {
-	chn := make(chan map[string][]string)
-
-	files := []string{}
-
-	err := filepath.Walk(dataset, func(path string, info os.FileInfo, err error) error {
-		abs, _ := filepath.Abs(path)
-
-		if strings.HasSuffix(info.Name(), ".txt") {
-			files = append(files, abs)
-		}
-
-		if err != nil {
-			log.Fatal(err)
-		}
-		return nil
-	})
-
-	if err != nil {
-		panic(err)
-	}
-
-	go func(fp []string) {
-		for _, f := range fp {
-			text, _ := ioutil.ReadFile(f)
-			m := make(map[string][]string)
-			m[f] = stopword.RemoveStopWords(string(text))
-
-			chn <- m
-		}
-		close(chn)
-	}(files)
-
-	for f := range chn {
-		for k, v := range f {
-			log.Printf("Doc: %s n: %d", k, len(v))
-			for _, w := range v {
-				w = stemmer.Stem(w)
-				log.Print(w)
-			}
-		}
-	}
+	log.Println("Hello")
 }
